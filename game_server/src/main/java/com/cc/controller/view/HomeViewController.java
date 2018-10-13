20533e9d265fc63df325e0f98edd2c392d67f6c1
chengkunchen <523061882@qq.com>
2018-10-13 17:28:44 +0800

    xxx

--- game_server/src/main/java/com/cc/controller/view/HomeViewController.java
+++ game_server/src/main/java/com/cc/controller/view/HomeViewController.java
@@ -1,6 +1,7 @@
 package com.cc.controller.view;
 
 import com.cc.common.annotation.IgnoreToken;
+import com.cc.model.user.UserModel;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,12 @@ public class HomeViewController {
     @IgnoreToken
     @GetMapping("/{page}")
     public ModelAndView index(@PathVariable String page, HttpServletRequest request, Model model){
+        // TODO: 2018/10/13  
+        request.getSession().setAttribute("token","e7259f68-0787-4bf0-a4f5-055b1b214979");
+        UserModel userModel = new UserModel();
+        userModel.setUserId(1L);
+        userModel.setUsername("zzzzz");
+        request.getSession().setAttribute("user",userModel);
         return new ModelAndView("ar/"+page,"model",model);
     }
 }
