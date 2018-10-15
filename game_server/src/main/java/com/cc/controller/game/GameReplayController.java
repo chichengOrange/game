41fb1f15a162c3cd6dfb1f555a78297261f2effd
chengkunchen <523061882@qq.com>
2018-10-15 13:56:17 +0800

    Update game_server/src/main/java/com/cc/controller/game/GameReplayController.java

--- game_server/src/main/java/com/cc/controller/game/GameReplayController.java
+++ game_server/src/main/java/com/cc/controller/game/GameReplayController.java
@@ -1,5 +1,7 @@
 package com.cc.controller.game;
 
+import com.cc.common.Utils.FileUtils;
+import com.cc.common.enums.ResultCode;
 import com.cc.common.result.PageResult;
 import com.cc.common.result.Result;
 import com.cc.controller.BaseController;
@@ -13,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -58,13 +64,23 @@ public class GameReplayController extends BaseController {
 
 
     @GetMapping({"/download"})
-    public Result download(@RequestParam  Long replayId) {
+    public Result download(@RequestParam  Long replayId, HttpServletRequest request, HttpServletResponse response) throws IOException {
         GameReplay replay = gameReplayService.selectByPrimaryKey(replayId);
 
         if (replay.getReplayContractId() != null){
             //去安存下载
         }
-        return successResult(replay);
+
+
+        //如果安存失败直接在服务器下载
+
+        File file = new File(replay.getReplayFile());
+        if (!file.exists()) {
+            return (new Result()).resultCode(ResultCode.NO_DATA).detail("replay不存在");
+        } else {
+            FileUtils.downLoad(file, request, response);
+            return this.successResult();
+        }
     }
 
 }
